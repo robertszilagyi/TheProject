33181ad80204e22b8206662d9e7c725dbe9d3a63
robert <robertszilagyi97@gmail.com>
2019-02-20 23:34:20 +0200

    Commit12

--- src/Main/java/com/fasttrackit/persistance/OrderedProductRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.fasttrackit.persistance;
-//
-//import com.fasttrackit.domain.OrderedProduct;
-//import org.springframework.data.repository.PagingAndSortingRepository;
-//
-//public interface OrderedProductRepository  extends PagingAndSortingRepository< OrderedProduct, Long>{
-//}
